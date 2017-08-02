dcf17e78ab00e25eef292a9bd15398f83eb2c361
Frank Wan <frankwan27@gmail.com>
2017-08-02 18:20:18 +0800

    Added scrollable autocomplete

--- src/providers/photon/photon.ts
+++ src/providers/photon/photon.ts
@@ -66,7 +66,7 @@ export class PhotonProvider {
       // We're using Angular HTTP provider to request the data,
       // then on the response, it'll map the JSON data to a parsed JS object.
       // Next, we process the data and resolve the promise with the new data.
-      this.http.get('https://photon.komoot.de/api/?q=' + query + '&limit=10')
+      this.http.get('https://photon.komoot.de/api/?q=' + query + '&limit=30')
         .map(res => res.json())
         .subscribe(data => {
           // we've got back the raw data, now generate the core schedule data
