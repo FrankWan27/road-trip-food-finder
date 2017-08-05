54cabbb6f370368808bb44c3411356f8df84da75
Frank Wan <frankwan27@gmail.com>
2017-08-05 23:48:34 +0800

    Changed to mapzen api for autocomplete, probably gonna change back

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -27,47 +27,46 @@ export class MapPage {
   }
  
   autocomplete(e: any) {
-    console.log(e);
     if (e.target.value != undefined) {
       this.photon.autocomplete(e.target.value).then(
         data => {
           // clear array
           this.places.length = 0;
-
           // don't know why i can't use data.features
           for (let i = 0; i < data["features"].length; i++) {
             // save some space
-            let place = data["features"][i].properties;
+            // let place = data["features"][i].properties;
 
-            // string to save
-            let string = "";
+            // // string to save
+            // let string = "";
 
-            if (place.name != undefined) {
-              string += place.name;
-              string += ", ";
-            }
+            // if (place.name != undefined) {
+            //   string += place.name;
+            //   string += ", ";
+            // }
 
-            if (place.street != undefined) {
-              string += place.street;
-              string += ", ";
-            }
+            // if (place.street != undefined) {
+            //   string += place.street;
+            //   string += ", ";
+            // }
 
-            if (place.city != undefined) {
-              string += place.city;
-              string += ", ";
-            }
+            // if (place.city != undefined) {
+            //   string += place.city;
+            //   string += ", ";
+            // }
 
-            if (place.state != undefined) {
-              string += place.state;
-              string += ", ";
-            }
+            // if (place.state != undefined) {
+            //   string += place.state;
+            //   string += ", ";
+            // }
 
-            if (place.country != undefined) {
-              string += place.country;
-            }
+            // if (place.country != undefined) {
+            //   string += place.country;
+            // }
 
             // add to array
-            this.places[i] = string;
+            this.places[i] = data["features"][i].properties.label;
+
           }
         }
       );
--- src/providers/photon/photon.ts
+++ src/providers/photon/photon.ts
@@ -11,7 +11,11 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class PhotonProvider {
   
+  public mapzenKey: string = "mapzen-5ZktmUZ";
   public coords: any;
+  public focusLng: number = -122.4;
+  public focusLat: number = 37.7;
+  public source: string = "osm";
 
   constructor(public http: Http) {
     this.coords = [];
@@ -19,41 +23,59 @@ export class PhotonProvider {
 
   searchRoute(start: any, end: any) {
     //1.
-    console.log("starting task");
+    console.log("Pulling coords from " + start + " and " + end);
 
     return new Promise(resolve => {
       // We're using Angular HTTP provider to request the data,
       // then on the response, it'll map the JSON data to a parsed JS object.
       // Next, we process the data and resolve the promise with the new data.
-      this.http.get('https://photon.komoot.de/api/?q=' + start + '&limit=1')
+      this.http.get('https://search.mapzen.com/v1/autocomplete?text=' + start + '&sources=' + this.source + '&focus.point.lat=' + this.focusLat + '&focus.point.lon=' + this.focusLng + '&api_key=' + this.mapzenKey)
         .map(res => res.json())
         .subscribe(data => {
-            console.log(data.features[0].geometry.coordinates[0]);
-            console.log(data.features[0].geometry.coordinates[1]);
-
-
-            //can make this an array
-            this.coords[0] = data.features[0].geometry.coordinates[0];
-            this.coords[1] = data.features[0].geometry.coordinates[1];
-
+          
+          this.coords[0] = data["features"][0].geometry.coordinates[0];
+          this.coords[1] = data["features"][0].geometry.coordinates[1];
         });
 
-        this.http.get('https://photon.komoot.de/api/?q=' + end + '&limit=1')
+      this.http.get('https://search.mapzen.com/v1/autocomplete?text=' + end + '&sources=' + this.source + '&focus.point.lat=' + this.focusLat + '&focus.point.lon=' + this.focusLng + '&api_key=' + this.mapzenKey)
         .map(res => res.json())
         .subscribe(data => {
-
-            console.log(data.features[0].geometry.coordinates[0]);
-            console.log(data.features[0].geometry.coordinates[1]);
-            //can make this an array
-            this.coords[2] = data.features[0].geometry.coordinates[0];
-            this.coords[3] = data.features[0].geometry.coordinates[1];
-            //3. 
-            resolve(this.coords);
-         console.log("finding path between these coords: " + this.coords);
-
+          
+          this.coords[2] = data["features"][0].geometry.coordinates[0];
+          this.coords[3] = data["features"][0].geometry.coordinates[1];
+          console.log(this.coords);
+          resolve(this.coords);
         });
-        //2.
-        console.log("ended task");
+            
+      // this.http.get('https://photon.komoot.de/api/?q=' + start + '&limit=1')
+      //   .map(res => res.json())
+      //   .subscribe(data => {
+      //       console.log(data.features[0].geometry.coordinates[0]);
+      //       console.log(data.features[0].geometry.coordinates[1]);
+
+
+      //       //can make this an array
+      //       this.coords[0] = data.features[0].geometry.coordinates[0];
+      //       this.coords[1] = data.features[0].geometry.coordinates[1];
+
+      //   });
+
+      //   this.http.get('https://photon.komoot.de/api/?q=' + end + '&limit=1')
+      //   .map(res => res.json())
+      //   .subscribe(data => {
+
+      //       console.log(data.features[0].geometry.coordinates[0]);
+      //       console.log(data.features[0].geometry.coordinates[1]);
+      //       //can make this an array
+      //       this.coords[2] = data.features[0].geometry.coordinates[0];
+      //       this.coords[3] = data.features[0].geometry.coordinates[1];
+      //       //3. 
+      //       resolve(this.coords);
+      //    console.log("finding path between these coords: " + this.coords);
+
+      //   });
+      //   //2.
+      //   console.log("ended task");
 
 
     });
@@ -66,38 +88,48 @@ export class PhotonProvider {
       // We're using Angular HTTP provider to request the data,
       // then on the response, it'll map the JSON data to a parsed JS object.
       // Next, we process the data and resolve the promise with the new data.
-      this.http.get('https://photon.komoot.de/api/?q=' + query + '&limit=30')
-        .map(res => res.json())
-        .subscribe(data => {
-          // we've got back the raw data, now generate the core schedule data
-          // and save the data for later reference
-          resolve(data);
 
-          for(let i = 0; i < data.features.length; i++)
-          {
-            let name = '', street = '', city = '', state = '', country = '';
+    this.http.get('https://search.mapzen.com/v1/autocomplete?text=' + query + '&sources=' + this.source + '&focus.point.lat=' + this.focusLat + '&focus.point.lon=' + this.focusLng + '&api_key=' + this.mapzenKey)
+      .map(res => res.json())
+      .subscribe(data => {
+        resolve(data);
 
-            if(data.features[i].properties.name != undefined)
-              name = 'Name: ' + data.features[i].properties.name;
-            
-            if(data.features[i].properties.street != undefined) 
-              street = ', Street: ' + data.features[i].properties.street;
-            
-            if(data.features[i].properties.city != undefined)
-              city = ', City: ' + data.features[i].properties.city;
-            
-            if(data.features[i].properties.state != undefined)
-              state = ', State: ' + data.features[i].properties.state;
-            
-            if(data.features[i].properties.country != undefined)
-              country = ', Country: ' + data.features[i].properties.country;
-
-            // if(name == city) state = '';
-            // if(state == country) country = '';
-
-            console.log(name + street + city + state + country);
-          }
-        });
+        console.log(data);
+      });
     });
+
+    //   this.http.get('https://photon.komoot.de/api/?q=' + query + '&limit=30')
+    //     .map(res => res.json())
+    //     .subscribe(data => {
+    //       // we've got back the raw data, now generate the core schedule data
+    //       // and save the data for later reference
+    //       resolve(data);
+
+    //       for(let i = 0; i < data.features.length; i++)
+    //       {
+    //         let name = '', street = '', city = '', state = '', country = '';
+
+    //         if(data.features[i].properties.name != undefined)
+    //           name = 'Name: ' + data.features[i].properties.name;
+            
+    //         if(data.features[i].properties.street != undefined) 
+    //           street = ', Street: ' + data.features[i].properties.street;
+            
+    //         if(data.features[i].properties.city != undefined)
+    //           city = ', City: ' + data.features[i].properties.city;
+            
+    //         if(data.features[i].properties.state != undefined)
+    //           state = ', State: ' + data.features[i].properties.state;
+            
+    //         if(data.features[i].properties.country != undefined)
+    //           country = ', Country: ' + data.features[i].properties.country;
+
+    //         // if(name == city) state = '';
+    //         // if(state == country) country = '';
+
+    //         console.log(name + street + city + state + country);
+    //       }
+    //     });
+    // });
   }
 }
